2ad7d6104acaed649163f9d70ae641b1a3b679db
Christian <ch.braendli@gmail.com>
2021-02-21 00:15:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
