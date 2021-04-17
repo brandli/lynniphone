2a6b8d2e4cf3b61668b3b9d5f11d7f2076748c44
Christian <ch.braendli@gmail.com>
2021-04-17 15:31:37 +0200

    debugging test

--- src/init/init.py
+++ src/init/init.py
@@ -1,3 +1,5 @@
 #! /usr/bin/env python
 from subprocess import call
-call(['espeak "Hello Lynn" 2>/dev/null'], shell=True)
+while(True) 
+    call(['espeak "Hello Lynn" 2>/dev/null'], shell=True)
+    time.sleep(30)
